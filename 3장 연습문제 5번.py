a990af159b590eb7a5595a1b0485224dd27ceb75
혜리 <dbjq24@gmail.com>
2025-10-07 14:10:09 +0900

    두 개의 정수 계산
    max.min() 사용하기

---
+++ 3장 연습문제 5번.py
@@ -0,0 +1,15 @@
+#4주차_3장 연습문제 1번
+#수학 수식을 파이썬 문법에 맞는 수식으로 작성하기
+
+
+#정수 입력
+x = int(input("x : "))
+y = int(input("y : "))
+
+# 출력 
+print ("x + y = ", x + y)
+print ("x - y = ", x - y)
+print ("x * y = ", x * y)
+print ("평균 : ", (x + y) /2)
+print ("큰 수 : ", max(x, y))
+print ("작은 수 : ", min(x, y))
